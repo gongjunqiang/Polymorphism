5804036edd7b6557650803079ef144f2e99fab8e
龚均强 <1208754526@qq.com>
2019-08-08 22:35:50 +0800

    111

--- Generic/Demo.cs
+++ Generic/Demo.cs
@@ -47,7 +47,7 @@ namespace Generic
 
     }
 
-    #region 泛型中default关键字的使用
+    #region 泛型中default关键字的使用：主要用于给泛型变量赋值
     public class Generic<T1,T2>
     {
 
@@ -59,14 +59,66 @@ namespace Generic
         {
             //data1 =new T1();//因为T1的类型不能确定是否未引用类型，所以不能直接使用关键字New
             //data1 = null;//值类型有时候不能未null
-            data1 = default(T1);
+            data1 = default(T1);//如果T1是引用类型，则赋值位bull，如果是值类型，就给默认值
             data2 = default(T2);
         }
 
 
     }
 
-
     #endregion
 
+    #region 泛型约束
+
+    public class GenericConstraint<T1, T2, T3>
+        where T1:struct//约束T1必须是值类型
+        where T2:class//约束T2必须是引用类型
+        where T3:new()//约束T3这个类型必须有一个无参数构造方法，这个约束必须放在约束的最后面
+
+
+        //其他类型：基类型、接口类型
+    {
+        public List<T2> ProductList { get; set; }
+
+        public T3 Publish { get; set; }
+
+        public GenericConstraint()
+        {
+            //this.ProductList = new List<T2>();
+            //Publish = new T3();//因为泛型中没有规定这个类型有无无参数构造方法，所以不能直接使用，加上约束后可以使用
+        }
+
+        public T2 ByCourse(T1 position)
+        {
+            //var index = position;
+
+            dynamic index = position;//动态类型：再运行时解析对象的类型，再编译阶段不解析
+            return this.ProductList[index];
+        }
+    }
+
+
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public int Id { get; set; }
+
+    }
+
+    public class Teacher
+    {
+
+        public string TeacherName { get; set; }
+        public int CourseCount { get; set; }
+
+        //public Teacher()
+        //{ }
+
+    }
+    #endregion
 }
+
+
+
+
+
--- Generic/Program.cs
+++ Generic/Program.cs
@@ -12,6 +12,21 @@ namespace Generic
         static void Main(string[] args)
         {
             TestStack();
+
+            TestGenericConstraint();
+            Console.WriteLine("--------------委托技术-------------");
+            //【3】定义委托变量
+            Lecturedelegate1 lecture1 =null;//引用类型
+            //【4】将委托变量与具体方法关联
+            lecture1 = ChangeLecture1;
+
+            lecture1 += ZhangeLecture1;
+            lecture1 += WangeLecture1;
+
+
+            //【5】通过委托变量使用方法
+            //lecture1();
+            lecture1.Invoke();//委托特有的Invoke()
             Console.ReadLine();
 
         }
@@ -38,5 +53,47 @@ namespace Generic
             Console.WriteLine(deom.Pop());
             Console.WriteLine(deom.Pop());
         }
+
+
+        /// <summary>
+        /// 测试泛型约束
+        /// </summary>
+        static void TestGenericConstraint()
+        {
+            GenericConstraint<int, Course, Teacher> generic = new GenericConstraint<int, Course, Teacher>();
+
+            //给对象属性赋值
+            generic.ProductList = new List<Course>
+            {
+                new Course{ CourseName = "英语",Id=1000},
+                 new Course{ CourseName = "数学",Id=1000},
+                  new Course{ CourseName = "物理",Id=1000}
+            };
+            generic.Publish = new Teacher() { TeacherName = "常老师", CourseCount = 3 };
+
+            var course = generic.ByCourse(0);
+            Console.WriteLine($"课程老师：{generic.Publish.TeacherName},课程Id{course.Id},课程名称{course.CourseName}");
+        }
+
+        //【2】根据委托编写方法
+        static void ChangeLecture1()
+        {
+            Console.WriteLine("委托技术");
+        }
+
+        static void ZhangeLecture1()
+        {
+            Console.WriteLine("Zhange委托技术");
+        }
+
+        static void WangeLecture1()
+        {
+            Console.WriteLine("Zhange委托技术");
+        }
+
+
     }
+
+    //声明委托(声明方法的原型：方法的返回值与参数个数)
+    public delegate void Lecturedelegate1();
 }
